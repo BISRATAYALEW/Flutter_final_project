132abfcf48672fc1a7335764502a7e3daf2dfa9b
Elshadaimelesse <elshadaimelesse@gmail.com>
2023-06-09 01:02:13 -0700

    first commit

---
+++ lib/game_page.dart
@@ -0,0 +1,192 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  static const String PLAYER_X = "X";
+  static const String PLAYER_Y = "O";
+
+  late String currentPlayer;
+  late bool gameEnd;
+  late List<String> occupied;
+
+  @override
+  void initState() {
+    initializeGame();
+    super.initState();
+  }
+
+  void initializeGame() {
+    currentPlayer = PLAYER_X;
+    gameEnd = false;
+    occupied = ["", "", "", "", "", "", "", "", ""];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Image.asset(
+          'image/bg.jpeg',
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _headerText(),
+              _gameContainer(),
+              _restartButton(),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+
+  Widget _headerText() {
+    return Column(
+      children: [
+        const Text(
+          "Tic Tac Toe",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "$currentPlayer turn",
+          style: const TextStyle(
+            color: Color(0xFFFFD700),
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _gameContainer() {
+    return Container(
+      decoration: BoxDecoration(color: Colors.greenAccent),
+      height: MediaQuery.of(context).size.height / 2,
+      width: MediaQuery.of(context).size.height / 2,
+      margin: const EdgeInsets.all(8),
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: 9,
+          itemBuilder: (context, int index) {
+            return _box(index);
+          }),
+    );
+  }
+
+  Widget _box(int index) {
+    return InkWell(
+      onTap: () {
+        //on click of box
+        if (gameEnd || occupied[index].isNotEmpty) {
+          //Return if game already ended or box already clicked
+          return;
+        }
+
+        setState(() {
+          occupied[index] = currentPlayer;
+          changeTurn();
+          checkForWinner();
+          checkForDraw();
+        });
+      },
+      child: Container(
+        color: occupied[index].isEmpty
+            ? Colors.black26
+            : occupied[index] == PLAYER_X
+                ? Color.fromARGB(255, 29, 159, 29)
+                : Color(0xFFFFD700),
+        margin: const EdgeInsets.all(8),
+        child: Center(
+          child: Text(
+            occupied[index],
+            style: const TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _restartButton() {
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            initializeGame();
+          });
+        },
+        child: const Text("Restart Game"));
+  }
+
+  changeTurn() {
+    if (currentPlayer == PLAYER_X) {
+      currentPlayer = PLAYER_Y;
+    } else {
+      currentPlayer = PLAYER_X;
+    }
+  }
+
+  checkForWinner() {
+    //Define winning positions
+    List<List<int>> winningList = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var winningPos in winningList) {
+      String playerPosition0 = occupied[winningPos[0]];
+      String playerPosition1 = occupied[winningPos[1]];
+      String playerPosition2 = occupied[winningPos[2]];
+      String _win = '';
+
+      if (playerPosition0.isNotEmpty) {
+        if (playerPosition0 == playerPosition1 &&
+            playerPosition0 == playerPosition2) {
+          //all equal means player won
+
+          gameEnd = true;
+          return;
+        }
+      }
+    }
+  }
+
+  checkForDraw() {
+    if (gameEnd) {
+      return;
+    }
+    bool draw = true;
+    for (var occupiedPlayer in occupied) {
+      if (occupiedPlayer.isEmpty) {
+        //at least one is empty not all are filled
+        draw = false;
+      }
+    }
+
+    if (draw) {
+      gameEnd = true;
+    }
+  }
+}
